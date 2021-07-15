d663d5e85062110efd30feaabead8afa6d9c338f
tom42 <tom42martin@gmail.com>
2021-07-14 23:09:34 -0400

    Update part1.ps1

--- scripts/part1.ps1
+++ scripts/part1.ps1
@@ -1,4 +1,18 @@
-#Requires -RunAsAdministrator
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
 
 Remove-Item -Path "c:\BGinfo" -Force -Recurse
 
