c22a8e91ffdc1e5fbf7bb88dbee9f6f7f544d1b7
tom42 <tom42martin@gmail.com>
2021-07-14 22:41:02 -0400

    Update part1.ps1

--- scripts/part1.ps1
+++ scripts/part1.ps1
@@ -1,3 +1,5 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 Remove-Item -Path "c:\BGinfo" -Force -Recurse
 
 New-Item -Path "c:\" -Name "WORK" -ItemType "directory"
