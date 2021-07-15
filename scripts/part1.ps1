9812765abe042794272730d33da90bf66dd18f8c
tom42 <tom42martin@gmail.com>
2021-07-15 02:01:22 -0400

    Update part1.ps1

--- scripts/part1.ps1
+++ scripts/part1.ps1
@@ -1,10 +1,3 @@
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
-{  
-  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
-  Start-Process powershell -Verb runAs -ArgumentList $arguments
-  Break
-}
-pause
 function part1 {
     Remove-Item -Path "c:\BGinfo" -Force -Recurse
 
